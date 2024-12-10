b426219ca5983ce1e8a802a65a12e491047d102c
manuelebeh <emmanuelebehdev@gmail.com>
2024-12-10 09:39:14 +0000

    Adding accessors and mutators to our models

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,14 +4,14 @@ namespace App\Models;
 
 class Address
 {
-    public int $id;
-    public string $user_id;
-    public string $street;
-    public string $city;
-    public string $country;
-    public string $postal_code;
-    public string $created_at;
-    public string $updated_at;
+    protected int $id;
+    protected string $user_id;
+    protected string $street;
+    protected string $city;
+    protected string $country;
+    protected string $postal_code;
+    protected string $created_at;
+    protected string $updated_at;
 
     public function __construct(int $id, string $user_id, string $street, string $city, string $country, string $postal_code, string $created_at, string $updated_at)
     {
@@ -22,6 +22,78 @@ class Address
         $this->country = $country;
         $this->postal_code = $postal_code;
         $this->created_at = $created_at;
+        $this->updated_at = $updated_at;
+    }
+
+    // Getters
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): int
+    {
+        return $this->user_id;
+    }
+
+    public function getStreet(): string
+    {
+        return $this->street;
+    }
+
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    public function getPostalCode(): string
+    {
+        return $this->postal_code;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt(): string
+    {
+        return $this->updated_at;
+    }
+
+    // Setters
+    public function setUserId(int $user_id): void
+    {
+        $this->user_id = $user_id;
+    }
+
+    public function setStreet(string $street): void
+    {
+        $this->street = $street;
+    }
+
+    public function setCity(string $city): void
+    {
+        $this->city = $city;
+    }
+
+    public function setCountry(string $country): void
+    {
+        $this->country = $country;
+    }
+
+    public function setPostalCode(string $postal_code): void
+    {
+        $this->postal_code = $postal_code;
+    }
+
+    public function setUpdatedAt(string $updated_at): void
+    {
         $this->updated_at = $updated_at;
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 class User
 {
-    public int $id;
-    public string $first_name;
-    public string $last_name;
-    public string $email;
-    public string $created_at;
-    public string $updated_at;
+    protected int $id;
+    protected string $first_name;
+    protected string $last_name;
+    protected string $email;
+    protected string $created_at;
+    protected string $updated_at;
 
     public function __construct(int $id, string $first_name, string $last_name, string $email, string $created_at, string $updated_at)
     {
@@ -18,6 +18,58 @@ class User
         $this->last_name = $last_name;
         $this->email = $email;
         $this->created_at = $created_at;
+        $this->updated_at = $updated_at;
+    }
+
+    // Getters
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->first_name;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->last_name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt(): string
+    {
+        return $this->updated_at;
+    }
+
+    // Setters
+    public function setFirstName(string $first_name): void
+    {
+        $this->first_name = $first_name;
+    }
+
+    public function setLastName(string $last_name): void
+    {
+        $this->last_name = $last_name;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function setUpdatedAt(string $updated_at): void
+    {
         $this->updated_at = $updated_at;
     }
 }
